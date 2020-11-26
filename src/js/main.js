e1d3951ac73db3670f457efe1643e25b01cef48a
RusTaggs <udachin_2000@mail.ru>
2020-11-26 15:11:13 +0300

    new main page header

--- src/js/main.js
+++ src/js/main.js
@@ -1,88 +1,43 @@
 $(document).ready(function(){
 
     
-    var headerSwiper = new Swiper('.swiper-container__main', {
-        
-      spaceBetween: 0,
-      speed: 500,
-      
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-          type: 'fraction',
-        },
-       
-      
-        // Navigation arrows
-        navigation: {
-          nextEl: '.nxt_main',
-          prevEl: '.prv_main',
-        },
-      
-        // And if we need scrollbar
-        scrollbar: {
-          el: '.main-scrollbar',
-          hide: false,
-          dragSize: 'auto'
-        },
-       
-        
-    });
-    
-    var newsSwiper = new Swiper('.swiper-news-container', {
-      
-      effect: 'coverflow',
-     
-      spaceBetween: 50,
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: 'auto',
-      coverflowEffect: {
-        rotate: 20,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows : true,
-      },
-      scrollbar: {
-        el: '.news-scrollbar',
-        hide: false,
-      },
-      navigation: {
-          nextEl: '.nxt_news',
-          prevEl: '.prv_news',
-        },
-      pagination: {
-        el: '.swiper-news-pagination',
-        type: 'fraction'
-      },
-      
-    });
-    var projectsSwiper = new Swiper('.swiper-projects-container', {
-      slidesPerView: 3,
-      spaceBetween: 0,
-      slidesPerGroup: 3,
-      slideClass: 'project-slide',
-      
-      pagination: {
-        el: '.swiper-projects-pagination',
-        type: 'fraction'
-      },
-      scrollbar: {
-        el: '.projects-scrollbar',
-        hide: false,
-        dragSize: 'auto'
-      },
-      navigation: {
-        nextEl: '.nxt_projects',
-        prevEl: '.prv_projects',
-      },
-    });
-      
-    $('.project-slide').hover(function(){
-      $(this).find('.projects-bg-img').addClass('active')
+  var swiper = new Swiper(".swiper-container", {
+    spaceBetween: 150,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function(index, className) {
+        if (index > 10) {
+          return (
+            '<span class="' +
+            className +
+            '"><span class="fractions">' +
+            (index + 1) +
+            "</span></span>"
+          );
+        } else {
+          return (
+            '<span class="' +
+            className +
+            '"><span class="fractions">0' +
+            (index + 1) +
+            "</span></span>"
+          );
+        }
+      }
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+    $('#krasnoyarsk').hover(function() {
+        $('.titleMain').html('Проекта');
+        $('.proj-counter').html('4');
     }, function(){
-      $(this).find('.projects-bg-img').removeClass('active')
-    }
-    );
+      $('.titleMain').html('Проектов');
+        $('.proj-counter').html('409');
+    });
 });
